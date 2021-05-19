d328d1f2083c9b32c9a2dc3708ce185e1cfee58c
bschulke <brynschulke@gmail.com>
2021-05-19 18:02:59 -0500

    added title, simpler xpaths, fixed errors

--- get_ingredients.py
+++ get_ingredients.py
@@ -30,14 +30,17 @@ recipe = {}
 articler = driver.find_element_by_class_name("recipe")
 
 #get the recipe title
-title = articler.find_element_by_xpath('//*[@id="main-content"]/article/div[1]/header/div[1]/div[1]/h1').text
+#title = articler.find_element_by_xpath('//*[@id="main-content"]/article/div[1]/header/div[1]/div[1]/h1').text
+title = articler.find_element_by_xpath('//*[@data-testid="ContentHeaderHed"]').text
+
+# print(title.text)
 
 recipe["title"] = title # assign to recipe dictionary
 
 bkgd = articler.find_element_by_class_name("content-background")
 
 #ingredients_title= bkgd.find_element_by_xpath("/html/body/div[1]/div/main/article/div[2]/div[1]/div/div[3]")
-ingredients_title = bkgd.find_element_by_xpath('//*[@data-testid="IngredientList"]/div')
+ingredients_title = bkgd.find_element_by_xpath('//*[@data-testid="IngredientList"]')
 
 #print(ingredients_title.text)
 
@@ -46,8 +49,8 @@ servings_text = ingredients_title.find_element_by_tag_name("p").text
 #servings = servings_text.text.split(" ")[1]
 servings = [int(s) for s in servings_text.split() if s.isdigit()]
 
-print("serving size: ",servings[0])
-print(servings_text)
+# print("serving size: ",servings[0])
+# print(servings_text)
 
 recipe["servings"] = servings[0] #assign to recipe dictionary
 #print(servings_text) # yay this worked!
@@ -58,13 +61,28 @@ items_html = ingredients_title.find_elements_by_tag_name("div")
 
 portions, items = [], []
 
-for x in np.arange(0,len(portions_html)):    #we WERE skipping index 0 bc serving size, but think we fixed it
-    portions.append(portions_html[x].text)
-    items.append(items_html[x].text)
+# define "is there special equipment" in var special. Start with False
+special = False
+
+for x in np.arange(1,len(portions_html)):    #skipping index 0 bc serving size
+    if items_html[x].text.upper() == "SPECIAL EQUIPMENT":
+        special = True
+        break
+    else:
+        portions.append(portions_html[x].text)
+        items.append(items_html[x].text)
 
 ingredientsList = pd.DataFrame({"amount": portions, "ofThing": items})
 
 recipe["ingredients"] = ingredientsList
+
+# using x result from for loop on line 64
+if special == True:
+    special_equip = []
+    for y in np.arange((x+1),len(items_html)):
+        special_equip.append(items_html[y].text)
+    special_equip = pd.DataFrame({"Addt. Tools": special_equip})
+    recipe["special equipment"] = special_equip
 
 #print(ingredientsList)
 
@@ -72,7 +90,7 @@ prep_title = bkgd.find_element_by_xpath('//*[@data-testid="InstructionsWrapper"]
 
 #print(prep_title.text)
 
-prep_title = bkgd.find_element_by_xpath("/html/body/div[1]/div/main/article/div[2]/div[1]/div/div[4]")
+#prep_title = bkgd.find_element_by_xpath("/html/body/div[1]/div/main/article/div[2]/div[1]/div/div[4]")
 
 
 stepstxt_html = prep_title.find_elements_by_tag_name("p")
@@ -80,9 +98,15 @@ stepstitle_html = prep_title.find_elements_by_tag_name("h3")
 
 steps, steptitles = [], []
 
-for x in np.arange(0,len(stepstxt_html)):
-    print(x)
-    steptitles.append(stepstitle_html[x].text)
+# if stepstxt_html and stepstitle_html are not the same length,
+# we're assuming there will be more step descriptions than step step steptitles
+# so we've added an "if" statement to append an empty string if this happens
+
+for x in np.arange(0,len(stepstitle_html)):
+    if x == len(stepstitle_html):
+        steptitles.append("")
+    else:
+        steptitles.append(stepstitle_html[x].text)
     steps.append(stepstxt_html[x].text)
     #print(stepstitle_html[x].text)
     #print(stepstxt_html[x].text)
